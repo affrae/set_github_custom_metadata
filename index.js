0f5c481ec8cf6f5c1fe889c1e7dc21b70fe408f2
Daniel Figucio <745341+affrae@users.noreply.github.com>
2020-07-21 09:47:17 +1000

    updated v1 release

--- index.js
+++ index.js
@@ -7,15 +7,38 @@ async function run() {
   try {
     const issue = github.context.issue;
     const key = core.getInput('key');
+    const value = core.getInput('value');
     const myToken = core.getInput('myToken');
     const octokit = github.getOctokit(myToken);
+    let data = {}
+
     console.log(issue['owner']);
     console.log(issue['repo']);
     console.log(issue['number']);
+    console.log(key);
+    console.log(value);
 
     const body = (await octokit.issues.get({owner:issue['owner'],repo:issue['repo'],issue_number:issue['number']})).data.body;
 
     console.log(body);
+
+    body = body.replace(regex, (_, json) => {
+        data = JSON.parse(json)
+        return ''
+      })
+
+    if (!data) data = {}
+
+    if (typeof key === 'object') {
+      Object.assign(data, key)
+    } else {
+      data[key] = value
+    }
+
+    body = `${body}\n\n<!-- abm_metadata = ${JSON.stringify(data)} -->`
+
+    return github.issues.update({owner:issue['owner'],repo:issue['repo'],issue_number:issue['number'], body:body})
+
   } catch (error) {
     core.setFailed(error.message);
   }
